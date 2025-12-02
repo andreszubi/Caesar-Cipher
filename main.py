9bb06b9eaf000b9db0cdee8bc4cbccef5bb77e5c
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2025-12-01 23:55:53 -0500

    Add goodbye logo to farewell message in main.py and define bye_logo in art.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from art import logo
+from art import logo, bye_logo
 print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -45,4 +45,4 @@ while go_again:
     # If the user doesn't want to go again, set go_again to False.
     if restart == "no":
         go_again = False
-        print("Goodbye!")
+        print(bye_logo)
